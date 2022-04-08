734fbc03b2440bf9c207d16d07537d032b3afb6c
SajjadHussain2000 <sh267002@gmail.com>
2022-04-08 20:25:30 +0530

    add to cart basic funtionality added

--- Shopping-Cart-Php-Mysql/index.php
+++ Shopping-Cart-Php-Mysql/index.php
@@ -10,7 +10,19 @@ if(isset($_POST['add'])){
     //print_r($_POST['product_id']);
     if(isset($_SESSION['cart']))
     {
-        print_r($_SESSION['cart']); 
+        $item_array_id = array_column($_SESSION['cart'],"product_id");
+        if(in_array($_POST['product_id'],$item_array_id))
+        {
+            echo "<script>alert('product is already in the cart....!')</script>";
+            echo "<script>window.location = 'index.php'</script>";
+        }else{
+            $count = count($_SESSION['cart']);
+            $item_array = array(
+                'product_id'=>$_POST['product_id']
+            );
+            $_SESSION['cart'][$count] = $item_array;
+            print_r($_SESSION['cart']);
+        }
     }else{
         $item_array = array(
             'product_id'=>$_POST['product_id']
