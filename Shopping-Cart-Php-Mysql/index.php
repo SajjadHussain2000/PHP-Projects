df36db7dc511f762f579a8958808c249b7247bc7
SajjadHussain2000 <sh267002@gmail.com>
2022-04-08 20:14:10 +0530

    implementing session

--- Shopping-Cart-Php-Mysql/index.php
+++ Shopping-Cart-Php-Mysql/index.php
@@ -1,9 +1,24 @@
 <?php
+
+session_start();
 require_once('php/CreateDb.php');
 require_once('./php/component.php');
 
 //creating instance of CreateDb class
 $database = new CreateDb("ProductDb","producttb");
+if(isset($_POST['add'])){
+    //print_r($_POST['product_id']);
+    if(isset($_SESSION['cart']))
+    {
+        print_r($_SESSION['cart']); 
+    }else{
+        $item_array = array(
+            'product_id'=>$_POST['product_id']
+        );
+        $_SESSION['cart'][0] = $item_array;
+        print_r($_SESSION['cart']);
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -24,7 +39,7 @@ $database = new CreateDb("ProductDb","producttb");
        <?php
         $result = $database->getData();
         while($row=mysqli_fetch_assoc($result)){
-            component($row['product_name'],$row['product_price'],$row['product_image']);
+            component($row['product_name'],$row['product_price'],$row['product_image'],$row['id']);
         }
        ?>
     </div>
