29282312fd8c16d151b182715d5e734d3b04e86e
SajjadHussain2000 <sh267002@gmail.com>
2022-04-09 17:23:01 +0530

    cart counter implemented

--- Shopping-Cart-Php-Mysql/php/header.php
+++ Shopping-Cart-Php-Mysql/php/header.php
@@ -16,7 +16,16 @@
             <div class="navbar-nav">
                 <a href="cart.php" class="nav-item nav-link active">
                     <h5 class="px-5 cart">
-                        <i class="fas fa-shopping-cart"></i>Cart 0
+                        <i class="fas fa-shopping-cart"></i>  Cart 
+                        <!-- <span id="cart_count" class="text-warning bg-light">0</span> -->
+                        <?php
+                            if(isset($_SESSION['cart'])){
+                                $count = count($_SESSION['cart']);
+                                echo "<span id=\"cart_count\" class=\"text-warning bg-light\">$count</span>";
+                            }else{
+                                echo "<span id=\"cart_count\" class=\"text-warning bg-light\">0</span>";
+                            }
+                            ?>
                     </h5>
                 </a>
             </div>
